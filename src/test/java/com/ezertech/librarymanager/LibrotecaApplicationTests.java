833a2b576bcf906b4fff76c654b7d761508242de
José Armenta <jarmenta@extreme.com.co>
2026-02-12 07:49:27 -0500

    Primer commit

---
+++ src/test/java/com/ezertech/librarymanager/LibrotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezertech.librarymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
